7a32dbd478de9fbcbe18e0d60070f77cede73ee1
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-27 11:26:05 -0400

    lab completed

---
+++ lib/migrations/versions/bd4c857c0659_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: bd4c857c0659
+Revises: 
+Create Date: 2023-03-27 11:14:48.751521
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd4c857c0659'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
